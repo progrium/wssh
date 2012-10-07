e4aa9c5d86fca588ea1370363aaa20049192625c
Josh Guilfoyle <jasta@devtcg.org>
2012-10-07 10:48:55 -0700

    Transmit data as binary messages in all cases.
    This avoids any potential character munging going on with the data
    transmitted.

--- wssh/common.py
+++ wssh/common.py
@@ -21,10 +21,11 @@ def opened(websocket):
         fcntl.fcntl(sys.stdin, fcntl.F_SETFL, os.O_NONBLOCK)
         while True:
             wait_read(sys.stdin.fileno())
-            line = sys.stdin.readline()
-            if not line:
+            buf = sys.stdin.read(4096)
+            if len(buf) == 0:
                 break
-            websocket.send(line)
+            # XXX: Always send the data as binary, regardless of what it really is.
+            websocket.send(buf, True)
         # stdin has been closed, but we'll keep the websocket open regardless
         # per netcat's behaviour.  This makes it much easier to use standard
         # shell script piping along with this tool.
