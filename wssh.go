df812b5cf93ae1630eee251325ab99b086d75a37
Jeff Lindsay <progrium@gmail.com>
2012-12-26 19:12:22 -0800

    formatting

--- wssh.go
+++ wssh.go
@@ -1,13 +1,13 @@
 package main
 
 import (
-  "flag"
+	"code.google.com/p/go.net/websocket"
+	"flag"
 	"fmt"
 	"io"
-  "os"
-  "net"
+	"net"
 	"net/http"
-	"code.google.com/p/go.net/websocket"
+	"os"
 )
 
 var port *int = flag.Int("p", 23456, "Port to listen/connect on")
@@ -15,52 +15,51 @@ var ignoreEof *bool = flag.Bool("i", false, "Ignore EOF on STDIN")
 var listenMode *bool = flag.Bool("l", false, "Listen mode (run a server)")
 
 func attach(ws *websocket.Conn) chan bool {
-  eof := make(chan bool, 1)
-  go func() {
-    io.Copy(os.Stdout, ws)
-    eof <- true
-  }()
-  go func() {
-    io.Copy(ws, os.Stdin)
-    if !*ignoreEof {
-      eof <- true
-    }
-  }()
-  return eof
+	eof := make(chan bool, 1)
+	go func() {
+		io.Copy(os.Stdout, ws)
+		eof <- true
+	}()
+	go func() {
+		io.Copy(ws, os.Stdin)
+		if !*ignoreEof {
+			eof <- true
+		}
+	}()
+	return eof
 }
 
-
 func connect() {
-  origin := "http://localhost/"
-  url := fmt.Sprintf("ws://localhost:%d/", *port)
-  ws, err := websocket.Dial(url, "", origin)
-  defer ws.Close()
-  if err != nil {
-    panic("connect: " + err.Error())
-  }
-  <-attach(ws)
+	origin := "http://localhost/"
+	url := fmt.Sprintf("ws://localhost:%d/", *port)
+	ws, err := websocket.Dial(url, "", origin)
+	defer ws.Close()
+	if err != nil {
+		panic("connect: " + err.Error())
+	}
+	<-attach(ws)
 }
 
 func listen() {
-  l, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
-  if err != nil {
-    panic("listen: " + err.Error())
-  } else {
-    http.Serve(l, websocket.Handler(func(ws *websocket.Conn) {
-      defer l.Close()
-      <-attach(ws)
-    }))
-  }
+	l, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		panic("listen: " + err.Error())
+	} else {
+		http.Serve(l, websocket.Handler(func(ws *websocket.Conn) {
+			defer l.Close()
+			<-attach(ws)
+		}))
+	}
 }
 
 func main() {
-  flag.Parse()
-  fmt.Printf("http://localhost:%d/\n", *port)
+	flag.Parse()
+	fmt.Printf("http://localhost:%d/\n", *port)
 
-  if *listenMode {
-    listen()
-  } else {
-    connect()
-  }
+	if *listenMode {
+		listen()
+	} else {
+		connect()
+	}
 
 }
